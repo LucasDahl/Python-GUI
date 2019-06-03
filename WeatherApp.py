e23fae2f7f1c31fed3b2c2ec96f5a4a5ef87edb2
Lucas <LucasDahl425@yahoo.com>
2019-06-03 12:26:05 -0700

    Added a test button.

--- WeatherApp.py
+++ WeatherApp.py
@@ -4,5 +4,9 @@ import tkinter as tk
 # Setup the root
 root = tk.Tk()
 
+# Add a button to the root and pack it
+button = tk.Button(root, text = "Test Button", bg = "gray", fg = "red")
+button.pack()
+
 # Run the mianloop
 root.mainloop()
