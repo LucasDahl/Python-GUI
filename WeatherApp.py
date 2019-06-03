ef1822a5ef6b1934e02d23972ec5eb0e1a821c1e
Lucas <LucasDahl425@yahoo.com>
2019-06-03 12:33:43 -0700

    Added a frame and canvas.

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,8 +1,20 @@
 # import libraries
 import tkinter as tk
 
+# Set the height and width
+canvasHeight = 700
+canvasWidth = 800
+
 # Setup the root
 root = tk.Tk()
+
+# Setup the canvas
+canvas = tk.Canvas(root, height = canvasHeight, width = canvasWidth)
+canvas.pack()
+
+# Setup the frame and place it.
+frame = tk.Frame(root, bg = "blue")
+frame.place(relwidth = 1, relheight = 1)
 
 # Add a button to the root and pack it
 button = tk.Button(root, text = "Test Button", bg = "gray", fg = "red")
