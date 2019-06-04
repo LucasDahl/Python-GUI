84339c2e324a2ee743adbcfafca527e3daec59ed
Lucas <LucasDahl425@yahoo.com>
2019-06-03 17:53:54 -0700

    Reorganized the UI elements for a better UI.

--- WeatherApp.py
+++ WeatherApp.py
@@ -18,21 +18,21 @@ canvas.pack()
 
 # Setup the frame and place it.
 frame = tk.Frame(root, bg = "blue")
-frame.place(relx = 0.1, rely = 0.1, relwidth = 0.8, relheight = 0.8)
+frame.place(relx = 0.5, rely = 0.1, relwidth = 0.75, relheight = 0.1, anchor = "n")
 
 # UI Elements =========================================================
 
+# Add an entry bar
+entryBar = tk.Entry(frame, font = 40)
+entryBar.place(relwidth = 0.65, relheight = 1)
+
 # Add a button to the root and pack it
-button = tk.Button(frame, text = "Test Button", bg = "gray", fg = "red")
-button.pack(side = "left", fill = "both", expand = True)
+button = tk.Button(frame, text = "Test Button", font = 40)
+button.place(relx = 0.7, relheight = 1, relwidth = 0.3)
 
 # Add a label and pack it in
-label = tk.Label(frame, text = "Hello World!!", bg = "yellow")
-label.pack(side = "left", fill = "both")
-
-# Add an entry bar
-entryBar = tk.Entry(frame, bg = "green")
-entryBar.pack(side = "left", fill = "both")
+#label = tk.Label(frame, text = "Hello World!", bg = "yellow")
+#label.place(relx = 0.3, rely = 0, relwidth = 0.45, relheight = 0.25)
 
 # End UI Elements ======================================================
 
